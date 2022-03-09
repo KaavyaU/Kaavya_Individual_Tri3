4b2d59b0fb57ef040a7995ead647a8f9c622e9ec
Kaavya U <72475036+KaavyaU@users.noreply.github.com>
2022-03-09 18:35:13 +0000

    Completed initial draft of IntByReference. Needs hashmap?

--- IntByReference.java
+++ IntByReference.java
@@ -3,13 +3,32 @@ public class IntByReference {
   
     // Hack: create IntByReference, swapToLowHighOrder and toString methods
 
+    //IntByReference Constructor
+    public IntByReference(int value){
+      this.value = value;
+    }
+
+    //swapToLowHighOrder method - Switches values so they are in low to high order
+    public void swapToLowHighOrder (IntByReference b) {
+      if (this.value > b.value){
+        int temp = b.value;
+        b.value = this.value;
+        this.value = temp;
+      }
+    }
+
+    //Overriding existing toString method 
+    public String toString(IntByReference b){
+      return this.value + " " + b.value;
+    }
+  
     // static method that enables me to see numbers swapped by reference (before, after)
     public static void swapper(int n0, int n1) {
         IntByReference a = new IntByReference(n0);
         IntByReference b = new IntByReference(n1);
-        System.out.println("Before: " + a + " " + b);
+        System.out.println("Before: "+ a.toString(b));
         a.swapToLowHighOrder(b);  // conditionally build swap method to change values of a, b
-        System.out.println("After: " + a + " " + b);
+        System.out.println("After: " + a.toString(b));
         System.out.println();
     }
 
